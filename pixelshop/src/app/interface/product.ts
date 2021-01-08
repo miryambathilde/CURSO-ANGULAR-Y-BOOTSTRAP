5171de460d24cdb7d347598940909e565b883035
miryambathilde <mbathilde@gmail.com>
2021-01-08 14:33:14 +0100

    modelo + interface incluidos

---
+++ pixelshop/src/app/interface/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+ title?: string; //title siempre tiene que ser un string
+ desc?: string; //desc siempre tiene que ser un string
+ price?: number; //price siempre tiene que ser un number
+ picture?: string; //picture siempre tiene que ser un string
+
+}
